ea4ec06d023750de4adda8510c52406e0b4ba450
Gabriel Albuquerque <contato@gabtech.com.br>
2021-07-23 23:36:24 -0300

    projeto finalizado

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -20,7 +20,7 @@ document.querySelectorAll('.item').forEach(item => {
 
 function itemClick(event) {
   let item = event.target.getAttribute('data-item');
-  if(square[item] === '') {
+  if(playing && square[item] === '') {
     square[item] = playerTurn;
     renderSquare();
     togglePlayerTurn();
@@ -39,10 +39,8 @@ function reset() {
   }
 
   playing = true;
-
   renderSquare();
   renderInfo();
-
 }
 
 function renderSquare() {
@@ -50,6 +48,8 @@ function renderSquare() {
     let item = document.querySelector(`div[data-item=${i}]`);
     item.innerHTML = square[i];
   }
+
+  checkGame();
 }
 
 function renderInfo() {
@@ -60,4 +60,50 @@ function renderInfo() {
 function togglePlayerTurn() {
   playerTurn = (playerTurn === 'x') ? 'o' : 'x';
   renderInfo();
+}
+
+function checkGame() {
+  if(checkWinnerFor('x')) {
+    warning = 'O "x" venceu!';
+    playing = false;
+  }else if(checkWinnerFor('o')) {
+    warning = 'O "o" venceu!';
+    playing = false;
+  }else if(isFull()) {
+    warning = 'Deu empate';
+    playing = false;
+  }
+}
+
+function checkWinnerFor(playerTurn) {
+  let possibilities = [
+    'a1,a2,a3',
+    'b1,b2,b3',
+    'c1,c2,c3',
+
+    'a1,b1,c1',
+    'a2,b2,c2',
+    'a3,b3,c3',
+    
+    'a1,b2,c3',
+    'a3,b2,c1'
+  ];
+
+  for(let w in possibilities) {
+    let pArray = possibilities[w].split(',');
+    let hasWon = pArray.every(option => square[option] === playerTurn);
+    if(hasWon) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function isFull() {
+  for(let i in square) {
+    if(square[i] === '') {
+      return false;
+    }
+  }
+  return true;
 }
